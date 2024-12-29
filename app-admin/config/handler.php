c3208c3e0201b29b9c649c57d02d3c5e175e68db
ManaPHP Mark <manaphp@qq.com>
2024-12-29 22:15:51 +0800

    \ManaPHP\Http\ErrorHandler

--- app-admin/config/handler.php
+++ app-admin/config/handler.php
@@ -3,7 +3,6 @@ declare(strict_types=1);
 
 return [
     'ManaPHP\Http\HandlerInterface' => [
-        'class'       => 'ManaPHP\Mvc\Handler',
         'middlewares' => [
             \ManaPHP\Http\Middlewares\RequestIdMiddleware::class,
             \ManaPHP\Http\Middlewares\AuthorizationMiddleware::class,
