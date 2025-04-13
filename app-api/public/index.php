01d2c30a9cb4ccf79589cec37f74d34eedbbb85f
ManaPHP Mark <manaphp@qq.com>
2025-04-13 14:20:54 +0800

    \ManaPHP\Kernel

--- app-api/public/index.php
+++ app-api/public/index.php
@@ -1,5 +1,11 @@
 <?php
 
-require __DIR__ . '/../bootstrap.php';
+use ManaPHP\Kernel;
 
-bootstrap('ManaPHP\Http\ServerInterface');
+ini_set('memory_limit', -1);
+ini_set('default_socket_timeout', -1);
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$kernel = new Kernel();
+$kernel->start('ManaPHP\Http\ServerInterface');
